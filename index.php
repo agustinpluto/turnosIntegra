cd6bd0dab353a99b8bf5f9136a7b84d3e0df7270
Agustin PLuto <agustinpluto@gmail.com>
2023-02-21 18:13:15 -0300

    first

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Turnos Integra</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+    <!-- Contenido de su sitio web -->
+    
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
