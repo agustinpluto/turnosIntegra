eb176479fdc15ba6c269ecb3c2be73587ce2b836
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 17:53:21 -0300

    asd

--- procesar_registro.php
+++ procesar_registro.php
@@ -2,9 +2,24 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
+
+    echo $email.$password;
+
     include 'conexion.php';
+    
     if (!empty($email) && !empty($password)) {
+
         $query = "SELECT id, email, password FROM usuarios WHERE email = '$email'";
+        $result = mysqli_query($conexion, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "Email: " . $row["email"] . "<br>";
+            }
+        } else {
+            echo "No se encontraron resultados.";
+        }
+
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $email);
         $stmt->execute();
