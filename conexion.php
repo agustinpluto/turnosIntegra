7acbda7f5766d9ca480a4efa12c6b4e40cca0956
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 05:48:00 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -1,10 +1,18 @@
 <?php
-    // datos de conexión
-    $servidor = "localhost";
-    $usuario = "agustin";
-    $password = "Ireliagod1!";
-    $basedatos = "integra";
 
-    // conexión
-    $mysqli = new mysqli($servidor, $usuario, $password, $basedatos);
+// Incluimos el archivo de conexión a la base de datos
+// datos de conexión
+$servidor = "localhost";
+$usuario = "agustin";
+$password = "Ireliagod1!";
+$basedatos = "integra";
+
+$con = mysqli_connect($servidor, $usuario, $password, $basedatos); 
+// Si la conexión falla, aparece el error 
+if($con === false) { 
+  echo 'Ha habido un error <br>'.mysqli_connect_error(); 
+} else {
+  echo 'Conectado a la base de datos';
+}
+
 ?>
--- procesar_registro.php
+++ procesar_registro.php
@@ -1,18 +1,5 @@
 <?php
-// Incluimos el archivo de conexión a la base de datos
-// datos de conexión
-$servidor = "localhost";
-$usuario = "agustin";
-$password = "Ireliagod1!";
-$basedatos = "integra";
-
-$con = mysqli_connect($servidor, $usuario, $password, $basedatos); 
-// Si la conexión falla, aparece el error 
-if($con === false) { 
-  echo 'Ha habido un error <br>'.mysqli_connect_error(); 
-} else {
-  echo 'Conectado a la base de datos';
-}
+include 'conexion.php';
 
 // Obtenemos los valores del formulario
 $nombre = $_POST['nombre'];
