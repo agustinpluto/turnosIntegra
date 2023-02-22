4bc35d3f58eb53a7b0723a418f9d928a2afbb45c
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 00:17:35 -0300

    asd

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light custom-nav">
       <a class="navbar-brand text-black" href="/">Integra</a>
-      <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button> -->
+      </button>
       <div class="collapse navbar-collapse" id="navbarNav">
 
         <ul class="navbar-nav mx-4">
