bf4b337ecefb745a1b54cb3d221f2cb412d479b7
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 16:38:57 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -4,10 +4,10 @@
     $username = 'agustin';
     $password = 'Ireliagod1!';
     $database = 'integra';
-    
+
     try {
       $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
-      echo "Conectado";
+      return $mensaje = "Conectado";
     } catch (PDOException $e) {
       die('Connection Failed: ' . $e->getMessage());
     }
