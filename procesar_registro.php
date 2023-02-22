5f5e850fe3874761a26ec91b3d8363edbaceedbd
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 05:22:46 -0300

    asd

--- procesar_registro.php
+++ procesar_registro.php
@@ -1,5 +1,12 @@
 <?php
 
+    // Obtener los valores de los campos del formulario
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $dni = $_POST['dni'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
     function insertarUsuario($nombre, $apellido, $dni, $email, $password) {
         include "conexion.php";
 
@@ -13,6 +20,16 @@
             return false;
         }
     }
+
+    // Llamar a la función insertarUsuario() con los valores correspondientes
+    if (insertarUsuario($nombre, $apellido, $dni, $email, $password)) {
+        // El usuario se ha registrado correctamente
+        header("Location: login.php");
+    } else {
+        // Ocurrió un error al registrar el usuario
+        echo "Ocurrió un error al registrar el usuario.";
+    }
+
 
 
 ?>
--- registro.php
+++ registro.php
@@ -1,25 +1,3 @@
-<?php
-
-    include "procesar_registro.php";
-
-    // Obtener los valores de los campos del formulario
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $dni = $_POST['dni'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-    // Llamar a la función insertarUsuario() con los valores correspondientes
-    if (insertarUsuario($nombre, $apellido, $dni, $email, $password)) {
-        // El usuario se ha registrado correctamente
-        header("Location: login.php");
-    } else {
-        // Ocurrió un error al registrar el usuario
-        echo "Ocurrió un error al registrar el usuario.";
-    }
-
-
-?>
 <!DOCTYPE html>
 <html>
 
