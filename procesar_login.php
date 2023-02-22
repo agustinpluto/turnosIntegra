ce9963cc20d11f4f406a26d6fcc9e02720630d98
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 18:08:22 -0300

    asd

--- procesar_login.php
+++ procesar_login.php
@@ -3,8 +3,6 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    echo $email.$password;
-
     include 'conexion.php';
     
     if (!empty($email) && !empty($password)) {
@@ -13,14 +11,11 @@
         $result = mysqli_query($conexion, $query);
 
         if (mysqli_num_rows($result) > 0) {
-            while($row = mysqli_fetch_assoc($result)) {
-                echo "Email: " . $row["email"] . "<br>";
-            }
+            header("Location: /loginOk.php");
         } else {
             echo "No se encontraron resultados.";
         }
 
-        header("Location: /loginOk.php");
 
       } else {
         echo "error";
