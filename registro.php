8b31eedbb6ca32f5764ae0c09bf6ee482dd61cbc
Agustin PLuto <agustinpluto@gmail.com>
2023-02-21 23:04:13 -0300

    asd

--- registro.php
+++ registro.php
@@ -35,7 +35,7 @@
         <div class="col">
           <div class="d-flex justify-content-center mt-5 mb-5">
             <form class="p-5 rounded" style="background-color: white; border: 2px solid #8800ff; border-radius: 10px;">
-              <h1 class="mb-4 text-center">Registro</h1>
+              <h1 class="mb-4 text-start text-danger">Registro</h1>
               <div class="row">
                 <div class="col">
                   <div class="mb-3">
