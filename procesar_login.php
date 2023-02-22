6670aa0dba73f4f3b39a3e3753dbb7287271d041
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 18:19:11 -0300

    asd

--- procesar_login.php
+++ procesar_login.php
@@ -9,10 +9,12 @@
     if (!empty($email) && !empty($password)) {
 
         $query = "SELECT id, email, password FROM usuarios WHERE email = '$email'";
+        echo $query;
         $stmt = mysqli_prepare($conexion, $query);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
+        echo $result;
         $user = mysqli_fetch_assoc($result);
         echo $user;
         if ($user && password_verify($password, $user["password"])) {
