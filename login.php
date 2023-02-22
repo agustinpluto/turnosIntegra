9bf59f210a3b53692798feee10e292d5b6dcee55
Agustin PLuto <agustinpluto@gmail.com>
2023-02-21 22:31:23 -0300

    asd

--- login.php
+++ login.php
@@ -33,9 +33,18 @@
     <div class="container">
       <div class="row">
         <div class="col">
-            <div class="container-fluid border border-danger border-radius-3">
-                <h1>asdhsad</h1>
+            <div class="d-flex justify-content-center aling-items-center pt-4 border rounded mx-5 mt-5">
+                <div class="card mx-2 mb-4">
+                <div class="card-header text-danger">
+                    Ingreso
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Registrarse</h5>
+                    <p class="card-text">Para crear su cuenta presione el boton "Registro"</p>
+                    <a href="/registro.php" class="btn btn-danger btn-block">Registro</a>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
