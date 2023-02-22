0392d459e93591e03324ab9a9d60ff17297cd615
Agustin PLuto <agustinpluto@gmail.com>
2023-02-22 18:16:49 -0300

    asd

--- procesar_login.php
+++ procesar_login.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -8,12 +9,23 @@
     if (!empty($email) && !empty($password)) {
 
         $query = "SELECT id, email, password FROM usuarios WHERE email = '$email'";
-        $result = mysqli_query($conexion, $query);
-        
-        header("Location: /loginOk.php");
-        
-      } else {
-        echo "error";
-      };
+        $stmt = mysqli_prepare($conexion, $query);
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $user = mysqli_fetch_assoc($result);
 
+        if ($user && password_verify($password, $user["password"])) {
+            $_SESSION["user_id"] = $user["id"];
+            header("Location: loginOk.php");
+            exit();
+        } else {
+            echo "Correo electrónico o contraseña incorrectos.";
+        }
+        
+    } else {
+        
+        echo "error";
+        
+    }
 ?>
